49217644ba955222fb045c4d090907adf517ee4b
zhouhuajun <zhouhuajun@burcent.com>
2018-10-20 18:21:24 +0800

    分支合到主干测试

---
+++ src/main/java/com/zhj/smvc/HelloTest.java
@@ -0,0 +1,20 @@
+/** 
+ * Filename HelloTest.java
+ * Copyright 2018 Jc All Rights Reserved.
+ */
+package com.zhj.smvc;
+
+
+/**
+ * Description: 
+ * 
+ * @author <a href="mailto:jackeychow2003@163.com">jackeychow</a>
+ * @date Create on 2018年10月20日 下午6:18:38
+ * @since version1.0
+ */
+public class HelloTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
