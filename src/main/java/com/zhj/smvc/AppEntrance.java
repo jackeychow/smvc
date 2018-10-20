f04b7d853da6f0bad197bcbdc9b2169ec1eaf35e
jackeychow <jackeychow2003@163.com>
2018-10-20 15:20:34 +0800

    测试java.ignore的相关配置

---
+++ src/main/java/com/zhj/smvc/AppEntrance.java
@@ -0,0 +1,23 @@
+/** 
+ * Filename AppEntrance.java
+ * Create on 2018年10月20日
+ * Copyright 2014 Jc All Rights Reserved.
+ */
+package com.zhj.smvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Description: 
+ * 
+ * @author <a href="mailto:275555302@qq.com">jackeychow</a>
+ * @since version1.0
+ */
+@SpringBootApplication
+public class AppEntrance {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppEntrance.class, args);
+    }
+}
--- src/main/java/com/zhj/smvc/SmvcApplication.java
+++ src/main/java/com/zhj/smvc/SmvcApplication.java
@@ -10,7 +10,7 @@ import lombok.extern.log4j.Log4j;
 public class SmvcApplication {
 
 	public static void main(String[] args) {
-	    log.info("配置DruidConfig后，采用lombok的log4j，启动主程序的时候，测试日志，进来了么？");
+	    log.info("配置DruidConfig后，采用lombok的log4j，启动主程序的时候，测试日志，进来了么？ yeah");
 		SpringApplication.run(SmvcApplication.class, args);
 	}
 }
